e04fe5aeb2a785ab309d1e9c22c0f7bfc7d20acb
Ajay <ajayreigns0@gmail.com>
2024-10-14 08:58:48 +0530

    fix: change port no.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import path from "path";
 import "dotenv/config";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
